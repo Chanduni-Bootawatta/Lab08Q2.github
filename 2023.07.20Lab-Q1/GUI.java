57f99750cbeadb109fdb5680550fc6fc9ccb7948
Chanduni-Bootawatta <136246845+Chanduni-Bootawatta@users.noreply.github.com>
2023-07-23 16:30:43 +0530

    Add files via upload

---
+++ 2023.07.20Lab-Q1/GUI.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui;
+
+/**
+ *
+ * @author sUBASHINI
+ */
+public class GUI {
+
+    public static void main(String[] args) 
+    {
+      rt c=new rt();
+       c.setTitle("ColorDemo");
+       c.show();
+    }
+
+}
---
+++ 2023.07.20Lab-Q2/GUI2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gui2;
+
+/**
+ *
+ * @author sUBASHINI
+ */
+public class GUI2 {
+
+    public static void main(String[] args) {
+        NewJFrame2 a= new NewJFrame2();
+        
+   a.setTitle("Click Counter");
+        
+        a.show();
+    }
+}
